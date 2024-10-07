365d37150dcaa5992e89b47c809307701a13f4f8
Shawn Sudaria <shawnlovecode14@gmail.com>
2024-10-08 00:29:31 +0800

    new

---
+++ Neetcode/AnagramGroups.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class AnagramGroups {
+
+    public Boolean isAnagram(String one, String two){
+        char[] one = one.toCharArray();
+        char[] two = two.toCharArray();
+        Boolean flag = true;
+        int 
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> group = new ArrayList<>();
+        
+        for(int i = 0; i < group.size(); i++){
+            
+            for(int j = i + 1; j < group.size(); i++){
+                
+            }
+        }
+
+
+        return group;
+    }
+
+    public static void main(String[] args){
+   
+        AnagramGroups solution = new AnagramGroups();
+        String[] strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println("Expected: [[\"eat\",\"tea\",\"ate\"],[\"tan\",\"nat\"],[\"bat\"]], Got: " + solution.groupAnagrams(strs1));
+        
+        }
+}
---
+++ Neetcode/problemTwo.java
@@ -0,0 +1,9 @@
+public class problemTwo {
+    public boolean isValid(String s){
+        
+
+    }
+    public static void main(String[] args){
+        problemTwo();                
+    }
+}
